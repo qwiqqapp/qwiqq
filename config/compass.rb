97c016262a0a3f842705581f287a05ae3a1c245b
Adam Maddox <adam@redcoded.com>
2011-05-06 15:47:49 -0700

    compass heroku adjustments

--- config/compass.rb
+++ config/compass.rb
@@ -1,1 +1,7 @@
-css_dir = "tmp/stylesheets/compiled"
+project_type = :rails
+project_path = Compass::AppIntegration::Rails.root
+environment  = Compass::AppIntegration::Rails.env
+
+http_path = '/'
+css_dir   = 'tmp/stylesheets'
+sass_dir  = 'app/stylesheets'
