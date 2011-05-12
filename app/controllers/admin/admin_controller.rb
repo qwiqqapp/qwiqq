ece0cacca0e397bd06a18bfa17220413f04de23c
Adam Maddox <adam@redcoded.com>
2011-05-11 18:16:02 -0700

    admin auth for admin area

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,6 +1,12 @@
 class Admin::AdminController < ActionController::Base
   
   layout 'admin'
+  before_filter :require_admin
   
-  
+  private
+  def require_admin
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "canucks" && password == "texasbbq"
+    end
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
    
   respond_to :json, :html 
-  
-  helper_method :current_user
 
-  private
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
     
 end
