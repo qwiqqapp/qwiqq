08aee7ad2dc068f71702a2948a40ec7ffa6a1f9f
Adam Maddox <adam@redcoded.com>
2011-05-30 10:58:54 -0700

    fixed test for categories

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -2,10 +2,9 @@ class Api::CategoriesController < Api::ApiController
   
   skip_before_filter :require_user
   
-  
   def show
     @category = Category.find_by_name!(params[:name])
-    @deals = @category.deals.order("created_at desc")
+    @deals    = @category.deals.order("created_at desc")
     
     render :json => @deals
   end
--- test/functional/categories_controller_test.rb
+++ test/functional/categories_controller_test.rb
@@ -6,16 +6,19 @@ class Api::CategoriesControllerTest < ActionController::TestCase
     assert_routing('/api/categories/travel', {:controller => "api/categories", :action => "show", :name => 'travel'})
   end
   
-  # test "should return deals for category" do
-  #   @user = Factory(:user)
-  #   sign_in(@user)
-  #   
-  #   @category = Factory(:category, :deals => [Factory(:deal), Factory(:deal)])
-  #   
-  #   get :show, :name => @category.name, :format => 'json'
-  #   
-  #   assert_equal 200, @response.status
-  #   assert_equal Array, json_response.class
-  #   assert_equal @category.deals.size, json_response.size
-  # end
+  test "should return deals for category" do
+    @user = Factory(:user)
+    sign_in(@user)
+    
+    @category = Factory(:category)
+    Factory(:deal, :category => @category)
+    Factory(:deal, :category => @category)
+    Factory(:deal, :category => @category)
+    
+    get :show, :name => @category.name, :format => 'json'
+    
+    assert_equal 200,   @response.status
+    assert_equal Array, json_response.class
+    assert_equal 3,     json_response.size
+  end
 end
