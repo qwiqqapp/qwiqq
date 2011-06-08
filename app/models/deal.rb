b80b99b1c50486527cbb8b528a4f99e3e1e7606f
Brian Collins <bricollins@gmail.com>
2011-06-08 11:55:44 +0100

    add short age to deal

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -37,7 +37,28 @@ class Deal < ActiveRecord::Base
       :lon            => lon.try(:to_s),
       :comment_count  => comment_count,
       :like_count     => like_count,
-      :age            => (created_at ? time_ago_in_words(created_at) : "")
+      :age            => (created_at ? time_ago_in_words(created_at) : ""),
+      :short_age      => short_created_at
     }
+  end
+
+
+
+  def short_created_at
+    from_time = created_at
+    to_time = Time.now
+    distance_in_minutes = (((to_time - from_time).abs)/60).round
+    distance_in_seconds = ((to_time - from_time).abs).round
+
+    case distance_in_minutes
+    when 0..1
+      "#{distance_in_seconds}s"
+    when 2..44           then "#{distance_in_minutes}m"
+    when 45..1439        then "#{(distance_in_minutes.to_f / 60.0).round}hr"
+    when 1440..43199     then "#{(distance_in_minutes.to_f / 1440.0).round}d"
+    when 43200..525599   then "#{(distance_in_minutes.to_f / 43200.0).round}mo"
+    else
+      "#{distance_in_minutes / 525600}y"
+    end
   end
 end
