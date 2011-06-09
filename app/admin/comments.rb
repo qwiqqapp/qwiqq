40ea0d9e1f8919f140e37f87fd5ce8569ae95f23
Adam Maddox <adam@redcoded.com>
2011-06-09 15:36:08 -0700

    updated admin closes #44 #45 #46

--- app/admin/comments.rb
+++ app/admin/comments.rb
@@ -2,19 +2,29 @@
 ActiveAdmin.register Comment, :as => "DealComment" do
   scope :all, :default => true
   scope :today
+  
+  actions :index, :show, :edit, :update, :destroy
 
   index do
     column("User", :sortable => :name) do |like|  
-      link_to(like.user.name, [ :admin, like.user ])
+      link_to(like.user.name, [ :admin, like.user ]) if like.user
     end
     
     column("Deal", :sortable => :name) do |like|  
-      link_to(like.deal.name, [ :admin, like.deal ])
+      link_to(like.deal.name, [ :admin, like.deal ]) if like.deal
     end
 
     column(:body)
     column(:created_at)
 
     default_actions
+  end
+  
+  form do |f|
+   f.inputs "Details" do
+     f.input :body
+   end
+
+   f.buttons
   end
 end
--- app/admin/likes.rb
+++ app/admin/likes.rb
@@ -1,17 +1,20 @@
 ActiveAdmin.register Like do
   scope :all, :default => true
   scope :today
+  
+  actions :index, :destroy
 
   index do
     column("User", :sortable => :name) do |like|  
-      link_to(like.user.name, [ :admin, like.user ])
+      link_to(like.user.name, [:admin, like.user]) if like.user
     end
     
     column("Deal", :sortable => :name) do |like|  
-      link_to(like.deal.name, [ :admin, like.deal ])
+      link_to(like.deal.name, [ :admin, like.deal ]) if like.deal
     end
 
     column(:created_at)
-    default_actions
+    
+    column(""){|like| link_to("Delete", [:admin, like], :method => 'delete')}
   end
 end
