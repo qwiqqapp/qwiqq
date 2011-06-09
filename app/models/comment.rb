3f311684154490d8866042ddab53b995969dbe94
Eoin Hennessy <eoin.hennessy@gmail.com>
2011-06-09 13:14:41 -0700

    keep comment and like counts on Deal current
    closes #25

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ActiveRecord::Base
-  
   belongs_to :deal
   belongs_to :user
 
@@ -9,9 +8,19 @@ class Comment < ActiveRecord::Base
   scope :today, lambda { where('DATE(created_at) = ?', Date.today)}
 
   after_create :deliver_notification
+  after_create :increment_comment_count
+  after_destroy :decrement_comment_count
 
   private
   def deliver_notification
     Notifications.deal_commented(self).deliver
+  end
+
+  def increment_comment_count
+    Deal.increment_counter(:comment_count, deal_id)
+  end
+   
+  def decrement_comment_count
+    Deal.decrement_counter(:comment_count, deal_id)
   end
 end
