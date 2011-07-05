5ad6162e66e043e8df3d6f65d82ac1a5c2ccfcdf
Eoin Hennessy <eoin.hennessy@gmail.com>
2011-07-05 11:38:57 -0700

    added actual deal posting to the users facebook wall

--- lib/qwiqq/facebook.rb
+++ lib/qwiqq/facebook.rb
@@ -1,20 +1,24 @@
 module Qwiqq
   module Facebook
     def self.share_deal(deal)
-      client = self.client_for_user(deal.user)
-
-      #client.put_wall_post( 
-      #  message, 
-      #  "name" => options[:link_name], 
-      #  "link" => options[:link], 
-      #  "caption" => options[:caption], 
-      #  "picture" => photo.thumbnail) 
+      return unless deal.shared_to_facebook_at.blank?
+      
+      client = client_for_user(deal.user)
+      client.put_wall_post( 
+        "I shared a deal on Qwiqq - #{deal.name}",
+        "name" => deal.name,
+        "link" => deal_url(deal),
+        "picture" => deal.photo.url(:admin_med))
 
       deal.update_attributes(:shared_to_facebook_at, Time.now)
     end
 
     def self.client_for_user(user)
-      graph = Koala::Facebook::GraphAPI.new(user.facebook_access_token)
+      Koala::Facebook::GraphAPI.new(user.facebook_access_token)
+    end
+
+    def self.deal_url(deal)
+      Rails.application.routes.url_helpers.deal_url(deal, :host => "qwiqq.me")
     end
   end
 end
