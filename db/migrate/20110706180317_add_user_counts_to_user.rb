99ed9db6ef2c95fd120d48b1ecc3f22ce1e85516
Eoin Hennessy <eoin.hennessy@gmail.com>
2011-07-06 13:04:08 -0700

    updated migration to make counts non-nullable
    this forces postgres to set existing columns to default

--- db/migrate/20110706180317_add_user_counts_to_user.rb
+++ db/migrate/20110706180317_add_user_counts_to_user.rb
@@ -1,8 +1,8 @@
 class AddUserCountsToUser < ActiveRecord::Migration
   def self.up
-    add_column :users, :followers_count, :int, :default => 0
-    add_column :users, :following_count, :int, :default => 0
-    add_column :users, :friends_count, :int, :default => 0
+    add_column :users, :followers_count, :int, :default => 0, :null => false
+    add_column :users, :following_count, :int, :default => 0, :null => false
+    add_column :users, :friends_count, :int, :default => 0, :null => false
   end
 
   def self.down
