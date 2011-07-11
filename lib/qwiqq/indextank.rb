cd9c0e5c835bbcd2776bed72c74208fc54c5bfd4
Adam Maddox <adam@adammaddox.com>
2011-07-11 14:06:22 -0700

    update

--- lib/qwiqq/indextank.rb
+++ lib/qwiqq/indextank.rb
@@ -1,3 +1,5 @@
+#  TODO: add faceting if required: http://indextank.com/documentation/tutorial-faceting
+
 # search query = deal name with distance
 # browse query = with category distance order
 
@@ -14,13 +16,20 @@
 # scoring fucntions/relevance
 # distance
 
+
 module Qwiqq
   module Indextank
     
     # attach document class to owner
-    def indextank
+    def indexed_doc
       @doc = Document.new(self)
     end
+    
+    def self.indextank
+      Document
+    end
+    
+    # -----------------------
     
     class Document
       attr_accessor :deal
@@ -32,7 +41,6 @@ module Qwiqq
       # will raise exception if fails to add document
       def add
         index.document(deal.id).add(fields, :variables => variables)
-        index.document(deal.id).update_categories(categories)
         deal.update_attribute(:indexed_at, Time.now)
         
       rescue IndexTank::InvalidArgument => e
@@ -50,6 +58,7 @@ module Qwiqq
       
       def fields
         fields = { :text         => deal.name,
+                   :category     => deal.category.name,
                    :image        => deal.photo.url(:iphone_list),
                    :image_2x     => deal.photo.url(:iphone_list_2x),
                    :timestamp    => deal.created_at.to_i}
@@ -57,42 +66,67 @@ module Qwiqq
          # conditionals
          fields[:price]   = deal.price    if deal.price
          fields[:percent] = deal.percent  if deal.percent
+         fields[:premium] = deal.premium  if deal.premium
+         
          fields
       end
-
+      
+      
       def variables
         { 0 => deal.lat,
           1 => deal.lon,
           2 => deal.like_count }
       end
+      
+      # allow deal to browse, and search
+      def self.browse(category, lat, long)
+        search('browse', {:q => "category:#{category}", :lat => lat, :long => long})
+      end
 
-      def categories
-        { :cat      => deal.category.name,
-          :premium  => deal.premium.to_s  }
+      def self.nearby(q, lat, long)
+        search('nearby', {:q => q, :lat => lat, :long => long})
+      end
+
+      def self.newest(q)
+        search('newest', {:q => q})
+      end
+
+      def self.popular(q)
+        search('popular', {:q => q})
       end
       
-
-
-
-      private
-      def functions
+      
+      def self.add_functions
         index.functions(0, '-age * relevance').add                  # Newest: newest and most relevant
         index.functions(1, "-miles(d[0], d[1], q[0], q[1])").add    # Nearby: location
-        index.functions(2, "log(d.var[2]) - age/86400").add         # Popular: like_count with age
+        index.functions(2, "log(doc.var[2]) - age/86400").add       # Popular: like_count with age
+      end
+      
+      # class method
+      def self.search(type, opts={})
+        q = opts[:q]
+        base = {:fetch => "text,image,image_2x,price,percent,premium"}
+        
+        function = case type
+          when 'newest'         then {:function => 0}
+          when /browse|nearby/  then {:function => 1, :var0 => opts[:lat], :var1 => opts[:long]}
+          when 'popular'        then {:function => 2}
+          else  
+            {}
+        end
+        
+        index.search(q, base.merge(function))['results']
       end
 
-      def client
+      private
+      def self.client
         @client = IndexTank::Client.new(ENV['INDEXTANK_API_URL'])
       end
 
-      def index
-        @index = client.indexes('deals')
+      def self.index
+        @index = self.client.indexes('deals')
       end
-      
-      
     end
-    
-    
   end
 end
 
