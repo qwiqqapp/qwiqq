5e92dada5ba79c6f9af839d3ce2fdeed9c3667df
Eoin Hennessy <eoin.hennessy@gmail.com>
2011-07-11 12:49:12 -0700

    use env for twitter config

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,14 +1,9 @@
 module Qwiqq
   def self.twitter_consumer_key
-    twitter_config["consumer_key"]
+    ENV["twitter_consumer_key"]
   end
 
   def self.twitter_consumer_secret
-    twitter_config["consumer_secret"]
+    ENV["twitter_consumer_secret"]
   end
-
-  private
-    def self.twitter_config
-      YAML.load_file(Rails.root.join("config", "twitter.yml"))[Rails.env]
-    end
 end
