3d8bd2c20636e860b635fc3dadbf9b1350a7c095
Adam Maddox <adam@adammaddox.com>
2011-07-12 12:54:04 -0700

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-
-
 ActiveRecord::Schema.define(:version => 20110711215235) do
 
   create_table "active_admin_comments", :force => true do |t|
