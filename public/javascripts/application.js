37c259e3569915c4c5d22e8d1924d4cfa5d35a72
Eoin Hennessy <eoin.hennessy@gmail.com>
2011-07-14 11:23:51 -0700

    make link colors dependant on scroll position

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,21 +1,43 @@
 $(function() {
+  var speed = 500;
+  var positions = {
+    top: 0,
+    app: 800,
+    deals: 1650,
+    contact: 2450
+  }
+
   $("a#top").click(function() {
-    $(document).scrollTo(0, 500);
+    $(document).scrollTo(positions.top, speed);
     return false;
   });
   
   $("a#app").click(function() {
-    $(document).scrollTo(800, 500);
+    $(document).scrollTo(positions.app, speed);
     return false;
   });
   
   $("a#deals").click(function() {
-    $(document).scrollTo(1650, 500);
+    $(document).scrollTo(positions.deals, speed);
     return false;
   });
   
   $("a#contact").click(function() {
-    $(document).scrollTo("max", 500);
+    $(document).scrollTo(positions.contact, speed);
     return false;
-  })
+  });
+
+  $(document).scroll(function() {
+    var top = $(document).scrollTop();
+    $("#navigation a").removeClass("active");  
+    if (top >= positions.contact) {
+      $("#navigation a#contact").addClass("active");
+    }
+    else if (top >= positions.deals) {
+      $("#navigation a#deals").addClass("active");
+    }
+    else if (top >= positions.app) {
+      $("#navigation a#app").addClass("active");
+    }
+  });
 });
