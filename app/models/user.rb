aa4863e2c75cc81a19b01a324ab6828639af85f0
Adam Maddox <adam@adammaddox.com>
2011-07-13 17:14:23 -0700

    require username

--- app/models/user.rb
+++ app/models/user.rb
@@ -65,7 +65,7 @@ class User < ActiveRecord::Base
   
   validates_confirmation_of :password
   validates_presence_of     :password, :on => :create
-  validates_presence_of     :email
+  validates_presence_of     :email, :username
   validates_uniqueness_of   :email, :username
   
   has_attached_file :photo, 
