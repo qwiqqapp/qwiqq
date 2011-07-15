fb73e87c33c48ccf630cc93d9b1a794ad3f303ef
Eoin Hennessy <eoin.hennessy@gmail.com>
2011-07-15 15:03:28 -0700

    added /iphone/about, /iphone/terms and /iphone/privacy
    closes #154

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+  layout :pick_layout
+  helper_method :iphone?
+  
   def about
     @jacks_deals = recent_deals("jack@qwiqq.me")
     @johns_deals = recent_deals("john@qwiqq.me")
@@ -18,5 +21,14 @@ class HomeController < ApplicationController
     def recent_deals(email)
       user = User.find_by_email(email)
       user.nil? ? [] : user.deals.limit(3)
+    end
+
+    def iphone?
+      @iphone
+    end
+
+    def pick_layout
+      @iphone = request.path =~ /iphone/
+      @iphone ? "iphone" : "application"
     end
 end
