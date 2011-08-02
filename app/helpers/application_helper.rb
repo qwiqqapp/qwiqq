5df9916569d3bd345aa0276a4281e6dadb8c7414
Brian Collins <brian@gastownlabs.com>
2011-08-02 12:15:15 +0100

    add emoji support to web. Closes #180

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def update_user_notifications_url(user)
     update_notifications_url(:token => user.notifications_token)
   end
+
+  def emojify(string)
+    string = HTMLEntities.new.encode(string, :hexadecimal)
+    string = string.gsub(/&#xe([^;]+);/m) do
+      "<span id=\"char\" class=\"emoji emoji_e#{$1}\"> </span>"
+    end
+    string.html_safe
+  end
 end
