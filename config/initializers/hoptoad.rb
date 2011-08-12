b28f46784126c0b9abb28014b005999a31509e83
Adam Maddox <adam@adammaddox.com>
2011-08-12 12:53:48 -0700

    added hoptoad api key as heroku config variable

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,3 +1,6 @@
+# production: 390aeca598c8e84e439e67a4034dcdd2
+# staging: 0ff186f437e676f2c6f7047f854b527b
+
 HoptoadNotifier.configure do |config|
-  config.api_key = '390aeca598c8e84e439e67a4034dcdd2'
+  config.api_key = ENV["HOPTOAD_API_KEY"]
 end
