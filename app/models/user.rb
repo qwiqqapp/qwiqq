02ce69ee5b748225f381c8ae86c1a9f4dd6f380d
Brian Collins <brian@gastownlabs.com>
2011-08-15 14:32:47 +0100

    fix double space in join date

--- app/models/user.rb
+++ app/models/user.rb
@@ -185,7 +185,7 @@ class User < ActiveRecord::Base
       :country             => country,
       :created_at          => created_at,
       :updated_at          => updated_at,
-      :join_date           => created_at.to_date.to_s(:long),
+      :join_date           => created_at.to_date.to_s(:long).gsub(/\s+/, " ")
       :send_notifications  => send_notifications,
       :facebook_authorized => !facebook_access_token.blank?,
       :twitter_authorized  => !twitter_access_token.blank?,
