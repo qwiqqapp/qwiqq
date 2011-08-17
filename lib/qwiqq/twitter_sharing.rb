3c377252bfc8d888c28f64ec88d68f6dd605b678
Brian Collins <brian@gastownlabs.com>
2011-08-17 15:36:08 +0100

    update twitter sharing urls

--- lib/qwiqq/twitter_sharing.rb
+++ lib/qwiqq/twitter_sharing.rb
@@ -5,14 +5,10 @@ module Qwiqq
     module InstanceMethods
       def share_deal_to_twitter(deal)
         # url for the post
-        deal_url = Rails.application.routes.url_helpers.deal_url(deal, 
-          :host => "www.qwiqq.me")
+        
 
         # build the message
-        message = Qwiqq.share_deal_message(deal, self).gsub(/qwiqq/i, "@Qwiqq") + " "
-        remaining_length = 140 - (message.length + deal_url.length + 1)
-        message += "#{deal.name.truncate(remaining_length)} #{deal_url}"
-
+        message = Qwiqq.twitter_message(deal, self)
         # post update
         twitter_client.update(message)
       end
