0db04653a6ac9b9c3c9b62ecd7451310c42dd8cc
Eoin Hennessy <eoin.hennessy@gmail.com>
2011-08-16 22:30:44 -0700

    execute bundle exec in cwd

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,7 @@ require "./config/boot"
 require "bundler/capistrano"
 require "hoptoad_notifier/capistrano"
 
+# an EC2 key is required
 raise "Environment variable 'EC2_KEY' is required." unless ENV["EC2_KEY"]
 
 set :application, "qwiqq"
@@ -15,20 +16,20 @@ set :deploy_via, :remote_cache
 
 role :app, "ec2-204-236-148-102.us-west-1.compute.amazonaws.com" #, "app1.qwiqq.me"
 
-set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
+set :unicorn_pid_path, "#{shared_path}/pids/unicorn.pid"
 set :unicorn_rails, "unicorn_rails"
 
 namespace :deploy do
   task :start, :roles => :app do
-    run "bundle exec #{unicorn_rails} -c #{deploy_to}/current/config/unicorn.rb -D -E production"
+    run "cd #{current_path} && bundle exec #{unicorn_rails} -c #{current_path}/config/unicorn.rb -D -E production"
   end
 
   task :stop, :roles => :app do
-    run "kill `cat #{unicorn_pid}`" if File.exists?(unicorn_pid)
+    run "kill `cat #{unicorn_pid_path}`" if File.exists?(unicorn_pid_path)
   end
   
   task :reload, :roles => :app do
-    run "kill -s USR2 `cat #{unicorn_pid}`" if File.exists?(unicorn_pid)
+    run "kill -s USR2 `cat #{unicorn_pid_path}`" if File.exists?(unicorn_pid_path)
   end
 
   task :restart, :roles => :app do
