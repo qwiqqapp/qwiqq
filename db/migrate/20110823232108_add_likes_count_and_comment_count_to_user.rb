e9aaa57cae025e485dc01c5c96d9cec95e1ff523
Brian Collins <brian@gastownlabs.com>
2011-08-24 00:37:14 +0100

    fix migration

--- db/migrate/20110823232108_add_likes_count_and_comment_count_to_user.rb
+++ db/migrate/20110823232108_add_likes_count_and_comment_count_to_user.rb
@@ -3,9 +3,9 @@ class AddLikesCountAndCommentCountToUser < ActiveRecord::Migration
     add_column :users, :likes_count, :integer, :default => 0
     add_column :users, :comments_count, :integer, :default => 0
     add_column :users, :deals_count, :integer, :default => 0
-    ActiveRecord::Base.connection.execute("update users set users.comments_count = (select count(id) from comments where comments.user_id = users.id)")
-    ActiveRecord::Base.connection.execute("update users set users.likes_count = (select count(id) from likes where likes.user_id = users.id)")
-    ActiveRecord::Base.connection.execute("update users set users.deals_count = (select count(id) from deals where deals.user_id = users.id)")
+    ActiveRecord::Base.connection.execute("update users set comments_count = (select count(id) from comments where comments.user_id = users.id)")
+    ActiveRecord::Base.connection.execute("update users set likes_count = (select count(id) from likes where likes.user_id = users.id)")
+    ActiveRecord::Base.connection.execute("update users set deals_count = (select count(id) from deals where deals.user_id = users.id)")
   end
 
   def self.down
