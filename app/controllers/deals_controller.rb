6c6158d3d16e6d23dbab4b19afc37199069579b2
Brian Collins <brian@gastownlabs.com>
2011-08-26 16:12:25 +0100

    cache web homepage views

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,4 +1,11 @@
 class DealsController < ApplicationController
+  caches_action :index, :expires_in => 10.minutes
+  caches_action :show, :if => lambda {|c| !c.ios? }, :cache_path => lambda {|c| "home/#{c.find_deal.cache_key}" }
+
+  def find_deal
+    @deal ||= Deal.find(params[:id])
+  end
+
   def index
     @deals = Deal.unscoped.order("likes_count desc, comments_count desc").limit(15)
     respond_with @deals
@@ -6,7 +13,6 @@ class DealsController < ApplicationController
   
   def show
     redirect_to "qwiqq:///deals/#{params[:id]}" and return if ios?
-    @deal = Deal.find(params[:id])
-    respond_with @deal
+    respond_with find_deal
   end
 end
