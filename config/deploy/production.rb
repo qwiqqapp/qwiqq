9a057d2916fc0adb3c5fccbea6dcc1b952ef9c83
Eoin Hennessy <eoin.hennessy@gmail.com>
2011-11-16 15:19:04 -0800

    removed capistrano config

--- config/deploy/production.rb
+++
@@ -1,6 +0,0 @@
-set :branch, "master"
-role :app, "app1.qwiqq.me", "app2.qwiqq.me"
-role :worker, "worker1.qwiqq.me"
-role :search, "worker2.qwiqq.me"
-role :db, "app1.qwiqq.me", :primary => true
-role :apn, "app1.qwiqq.me"
