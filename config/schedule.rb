3aca5e3a25209854845d0ca9a2cbe4744d52cedf
Adam Maddox <adam@adammaddox.com>
2011-11-15 16:54:54 -0800

    resque updates

--- config/schedule.rb
+++
@@ -1,9 +0,0 @@
-job_type :rake, "cd :path && PATH=/usr/local/bin:$PATH RAILS_ENV=:environment bundle exec rake :task :output"
-
-every 2.minutes do
-  rake "thinking_sphinx:index"
-end
-
-every 1.day, :at => "1:00 am" do 
-  rake "db:backup"
-end
