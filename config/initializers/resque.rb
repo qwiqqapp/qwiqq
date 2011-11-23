f8620feaf1b7624dc7782ff66551427d7e8485b4
Eoin Hennessy <eoin.hennessy@gmail.com>
2011-11-23 12:22:15 -0800

    establish db connection in workers after forking

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -5,4 +5,6 @@ require "resque/failure/airbrake"
 
 Resque::Failure::Multiple.classes = [ Resque::Failure::Redis, Resque::Failure::Airbrake ]
 Resque::Failure.backend = Resque::Failure::Multiple
-
+Resque.after_fork do |worker|
+  ActiveRecord::Base.establish_connection
+end
