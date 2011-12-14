4e6a2445a3a7992fe8b81983b42d7bbcbfc5acd0
Eoin Hennessy <eoin.hennessy@gmail.com>
2011-12-13 18:11:24 -0800

    first pass at user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,10 @@ class UsersController < ApplicationController
     @user.update_attributes(:send_notifications => params[:enable] || false)
     render :notifications_disabled
   end
+
+  def show
+    @user = User.find(params[:id])
+    # TODO pagination
+    @deals = @user.deals.first(5)
+  end
 end
