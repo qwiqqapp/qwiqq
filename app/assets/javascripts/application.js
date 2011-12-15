419109ea19ce31d7d745dac93a91e98b6c053ba8
Eoin Hennessy <eoin.hennessy@gmail.com>
2011-12-15 15:28:48 -0800

    design tweaks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,19 +2,22 @@
 //= require jquery.cycle.js
 
 $.fn.fixTo = function (pos) {
-  var $this = this, $window = $(window), top = this.position().top;
+  var $this = this, $window = $(window), position = $this.position();
+  if (!position) return;
   $window.scroll(function(e) {
     if ($window.scrollTop() > pos) {
-      $this.css({ position: "absolute", top: pos + top });
+      $this.css({ position: "absolute", top: pos + position.top });
     } else {
-      $this.css({ position: "fixed", top: top });
+      $this.css({ position: "fixed", top: position.top });
     }
   });
 };
 
 $(function() {
+  // cycle screenshots
   $("#screenshots").cycle("fade");
 
+  // free fixed divs at certain positions
   sliderHeight = $("#slider").height();
   infoHeight = $("#info").height();
   $("#info").fixTo(sliderHeight - infoHeight);
