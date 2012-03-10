5378af683171e9c7434d19d9b22a387710818421
Adam Maddox <adam@adammaddox.com>
2012-03-10 14:26:07 -0800

    serve missing images from s3

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,7 +1,9 @@
+# example missing url: http://qwiqq.images.production.s3.amazonaws.com/missing/users/iphone_zoom_2x.png
+
 PAPERCLIP_STORAGE_OPTIONS = {  :storage   => :s3, 
                                :bucket    => ENV['S3_BUCKET'],
                                :path      => ':class/:id/:style.:extension',
-                               :default_url =>   '/images/missing/:class/:style.png',
+                               :default_url =>   "http://#{ENV['S3_BUCKET']}.s3.amazonaws.com/missing/:class/:style.png",
                                :s3_credentials => { :access_key_id     => ENV['S3_KEY'], 
                                                     :secret_access_key => ENV['S3_SECRET'] } 
                             }
