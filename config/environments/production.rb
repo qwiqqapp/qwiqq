9fda4a4339b35372b48004ada2cbba7f598cdb46
Adam Maddox <adam@adammaddox.com>
2012-03-27 10:01:02 -0700

    move external assets to vendor. add basic manifest.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Qwiqq::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( mobile.css active_admin.css active_admin.js )
+  config.assets.precompile += %w( mobile.css active_admin.css active_admin.js basic.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
