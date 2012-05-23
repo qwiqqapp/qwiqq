2dab9f1a3310b0ce84c96c1874b23befb848baa3
Adam Maddox <adam@adammaddox.com>
2012-05-23 12:53:52 -0700

    readme cleanup. schema update.

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,32 @@ ActiveRecord::Schema.define(:version => 20120523181131) do
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
+  create_table "apn_devices", :force => true do |t|
+    t.string   "token",              :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.datetime "last_registered_at"
+    t.integer  "user_id"
+  end
+
+  add_index "apn_devices", ["token"], :name => "index_apn_devices_on_token", :unique => true
+  add_index "apn_devices", ["user_id"], :name => "index_apn_devices_on_user_id"
+
+  create_table "apn_notifications", :force => true do |t|
+    t.integer  "device_id",                        :null => false
+    t.integer  "errors_nb",         :default => 0
+    t.string   "device_language"
+    t.string   "sound"
+    t.string   "alert"
+    t.integer  "badge"
+    t.text     "custom_properties"
+    t.datetime "sent_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "apn_notifications", ["device_id"], :name => "index_apn_notifications_on_device_id"
+
   create_table "categories", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
