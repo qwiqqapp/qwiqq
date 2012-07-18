576513247da2a71d6120c724b844bcb554908e0c
Michael <michaelscaria@yahoo.com>
2012-07-17 19:23:07 -0500

    save

--- app/controllers/api/relationships_controller.rb
+++ app/controllers/api/relationships_controller.rb
@@ -5,16 +5,17 @@ class Api::RelationshipsController < Api::ApiController
       render :status => 405, :json => { :message => 'Unable to follow yourself' }
     else  
       relationship = current_user.follow!(target)
-      target_deals = target.deals.limit(10).order("deals.timestamp DESC")
-      return if target_deals
-      render :status => 405, :json => { :message => 'Cannot retrieve deals' }
-     end
-      
       render :status => :created, :json => {
         :followers_count => target.followers_count + 1,
         :following_count => target.following_count,
         :friends_count =>   target.friends_count }
-
+      target_deals = target.deals.limit(10).order("deals.timestamp DESC")
+      target_deals.each do |deal|
+        Feedlet.new(:user_id => current_user.id, 
+                  :deal_id => deal.id, 
+                  :posting_user_id => target.id, 
+                  :reposted_by => nil, 
+                  :timestamp => deal.created_at) end
       end
   end
 
