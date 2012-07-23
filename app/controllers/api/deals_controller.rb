41e3499d4de9243defe0a1f0fb341ef40b2a294d
Michael Scaria <michaelscaria@yahoo.com>
2012-07-23 15:51:26 -0500

    schedule reverted

--- app/controllers/api/deals_controller.rb
+++ app/controllers/api/deals_controller.rb
@@ -1,8 +1,5 @@
 class Api::DealsController < Api::ApiController
-  require 'rubygems'
-  require 'rufus/scheduler'
 
-  scheduler = Rufus::Scheduler.start_new
   
   skip_before_filter :require_user, :only => [:popular, :show, :index]
   caches_action :popular, :expires_in => 10.minutes
@@ -64,16 +61,7 @@ class Api::DealsController < Api::ApiController
     @deal.user = current_user
     @deal.save
     #30 DAYS
-    scheduler.every '21s' do |job|
-      l = current_user.deals_count + 1
-      if l >= current_user.deals_count
-        job.unschedule
-      else
-        #hasn't posted new deal in past month
-        Mailer.create_post(@user).deliver
-      end
-      
-    end
+
     respond_with @deal
   end
 
