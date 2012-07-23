111dc69b665b173f0cb10b477a0b54f94cb18ccf
Michael Scaria <michaelscaria@yahoo.com>
2012-07-23 18:57:07 -0500

    2 minute share email test

--- app/controllers/api/deals_controller.rb
+++ app/controllers/api/deals_controller.rb
@@ -61,7 +61,20 @@ class Api::DealsController < Api::ApiController
     @deal.category = category
     @deal.user = current_user
     @deal.save
-
+    #In 30 DAYS check to see if user has shared
+    scheduler = Rufus::Scheduler.start_new
+    #original deal count should be out of scope
+    original_deal_count = current_user.deals_count
+    scheduler.every '2m' do |job|
+      if @deal = current_user.deals.sorted[0] && original_deal_count >= current_user.deals_count
+        #user hasn't shared in past 30 days, send out missed email
+        Mailer.missed_email(current_user).deliver
+      else
+        #user has shared in past 30 days
+        Mailer.create_post(current_user).deliver
+        job.unschedule
+      end
+    end
     respond_with @deal
   end
 
