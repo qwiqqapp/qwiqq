2b282092b433600c3f3a3adf8ac4f66fb786280d
Michael Scaria <michaelscaria@yahoo.com>
2012-07-24 12:17:48 -0500

    test to see how share and welcome email look

--- app/controllers/api/deals_controller.rb
+++ app/controllers/api/deals_controller.rb
@@ -85,6 +85,8 @@ class Api::DealsController < Api::ApiController
   end
 
   def destroy
+    Mailer.share_post(current_user).deliver
+    Mailer.welcome_email(current_user).deliver
     @deal = current_user.deals.find(params[:id])
     @deal.destroy
     respond_with @deal
