d95be53f5024289dcd705f98eb1c98c7fb128db7
Michael Scaria <michaelscaria@yahoo.com>
2012-08-07 16:50:58 -0500

    add blank to country

--- app/controllers/api/deals_controller.rb
+++ app/controllers/api/deals_controller.rb
@@ -85,7 +85,21 @@ class Api::DealsController < Api::ApiController
   end
 
   def destroy
-
+    scheduler = Rufus::Scheduler.start_new
+   
+    #check if user has created profile in 1 day
+    user_email = current_user.email
+      scheduler.every '30s' do |job|
+        user = User.find_by_email(user_email)
+        return if user.nil?
+        if user.country.blank?
+          #user hasn't created a post yet, send email
+          Mailer.update_profile(user).deliver
+        else
+          #user has created a post
+          job.unschedule
+        end
+      end
     @deal = current_user.deals.find(params[:id])
     @deal.destroy
     respond_with @deal
