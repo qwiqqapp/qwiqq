44493461b341dac45805d26aa00a8940adf65341
Michael Scaria <michaelscaria@yahoo.com>
2012-08-07 10:42:54 -0500

    update profile test

--- app/controllers/api/deals_controller.rb
+++ app/controllers/api/deals_controller.rb
@@ -91,11 +91,12 @@ class Api::DealsController < Api::ApiController
       scheduler.every '15s' do |job|
         user = User.find_by_email("pardesiboyy@yahoo.com")
         return if user.nil?
-        Mailer.create_post(user).deliver
+        
         if user.first_name.blank? || user.last_name.blank?
           #user hasn't created a post yet, send email
           Mailer.update_profile(user).deliver
         else
+          Mailer.create_post(user).deliver
           #user has created a post
           job.unschedule
         end
