81a857fcb9767680a1a979f56ccc938ea5100ada
Michael Scaria <michaelscaria@yahoo.com>
2012-08-16 13:50:45 -0500

    removed Approximate Location completely from venue_or_location_name getter

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -266,7 +266,7 @@ class Deal < ActiveRecord::Base
   end
 
   def venue_or_location_name
-    foursquare_venue_name || location_name || 'Approximate Location'
+    foursquare_venue_name || location_name
   end
 
   def redeem_coupon!
@@ -283,7 +283,7 @@ class Deal < ActiveRecord::Base
     c = ""
     c << "An Awesome Qwiqq #coupon! " if self.coupon?
     c << self.price_as_string if self.price
-    c << " at #{venue_or_location_name}." unless venue_or_location_name.blank? && venue_or_location_name == 'Approximate Location'
+    c << " at #{venue_or_location_name}." unless venue_or_location_name.blank?
     c << " Posted by #{self.user.username}"
     c
   end
