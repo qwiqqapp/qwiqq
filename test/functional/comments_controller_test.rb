8dead6522170ee30d087483c28c17f41c676ec98
Michael Scaria <michaelscaria@yahoo.com>
2012-08-16 18:38:53 -0500

    testing custom constantcontact ruby

--- test/functional/comments_controller_test.rb
+++ test/functional/comments_controller_test.rb
@@ -1,118 +1,22 @@
 require 'test_helper'
 
-class Api::CommentsControllerTest < ActionController::TestCase
+class Api::ConstantcontactControllerTest < ActionController::TestCase
 
-  test "should route to comments#index for deals" do
-    assert_routing("/api/deals/1/comments.json", {
-      :format => "json", :controller => "api/comments", :action => "index", :deal_id => "1" })
-  end
-  
-  test "should route to comments#index for users" do
-    assert_routing("/api/users/1/comments.json", {
-      :format => "json", :controller => "api/comments", :action => "index", :user_id => "1" })
-  end
-  
-  test "should route to comments#create" do
-    assert_routing({ :method => "post", :path => "/api/deals/1/comments.json" }, {
-      :format => "json", :controller => "api/comments", :action => "create", :deal_id => "1" })
+   test "should route to constantcontact#index" do
+    assert_routing("/api/users/1/constantcontact.json", {
+      :format => "json", 
+      :controller => "api/invitations", 
+      :action => "index", 
+      :user_id => "1"})
   end
 
-  test "should routes to comments#destroy" do
-    assert_routing({ :method => "delete", :path => "/api/comments/1.json" }, {
-      :format => "json", :controller => "api/comments", :action => "destroy", :id => "1"})
-  end
-  
-  test "should return comments for a deal" do
-    @user = Factory(:user)
-    @deal = Factory(:deal)
-    sign_in(@user)
-    
-    @comment0 = Factory(:comment, :deal => @deal, :user => @user, :created_at => Time.now - 1.hour)
-    @comment1 = Factory(:comment, :deal => @deal, :user => @user, :created_at => Time.now - 2.hours)
-    
-    get :index, :deal_id => @deal.id, :format => "json"
-    
+  test "should render a constantcontact email" do
+    @user = User.find_by_email("michaelscaria26@gmail.com")
+
+    Mailer.constant_contact(@user).deliver
+
     assert_equal 200, @response.status
     assert_equal Array, json_response.class
-    assert_equal 2, json_response.size
-    
-    # content
-    assert_equal @comment0.id.to_s, json_response.first['comment_id']
-    assert_equal @user.id.to_s, json_response.first['user']['user_id']
+    assert_equal 1, json_response.size 
   end
-  
-  test "should return comments from a user" do
-    @user = Factory(:user)
-    sign_in(@user)
-    
-    @deal0 = Factory(:deal)
-    @deal1 = Factory(:deal)
-
-    @comment0 = Factory(:comment, :deal => @deal0, :user => @user, :created_at => Time.now - 1.hour)
-    @comment1 = Factory(:comment, :deal => @deal1, :user => @user, :created_at => Time.now - 2.hours)
-
-    get :index, :user_id => @user.id, :format => "json"
-    
-    assert_equal 200, @response.status
-    assert_equal Array, json_response.class
-    assert_equal 2, json_response.size
-    
-    # content
-    assert_equal @comment0.id.to_s, json_response.first['comment_id']
-  end
-
-  test "should return comments from the current user" do
-    @user = Factory(:user)
-    sign_in(@user)
-    
-    @deal0 = Factory(:deal)
-    @deal1 = Factory(:deal)
-
-    @comment0 = Factory(:comment, :deal => @deal0, :user => @user, :created_at => Time.now - 1.hour)
-    @comment1 = Factory(:comment, :deal => @deal1, :user => @user, :created_at => Time.now - 2.hours)
-
-    get :index, :user_id => "current", :format => "json"
-    
-    assert_equal 200, @response.status
-    assert_equal Array, json_response.class
-    assert_equal 2, json_response.size
-    
-    # content
-    assert_equal @comment0.id.to_s, json_response.first['comment_id']
-  end
-
-  test "should create a comment on a deal for the current user" do
-    @user = Factory(:user)
-    @deal = Factory(:deal)
-    sign_in(@user)
-
-    params = { :body => "This is the best deal ever." }
-
-    post :create, :deal_id => @deal.id, :comment => params, :format => "json"
-
-    assert_equal 201, @response.status
-  end
-
-  test "should fail to create an invalid comment" do
-    @user = Factory(:user)
-    @deal = Factory(:deal)
-    sign_in(@user)
-
-    post :create, :deal_id => @deal.id, :comment => {}, :format => "json"
-
-    # TODO shouldn't an invalid entity result in a 422?
-    assert_equal 400, @response.status
-  end
-
-  test "should delete a comment if it belongs to the current user" do
-    @user = Factory(:user)
-    @deal = Factory(:deal)
-    @comment = Factory(:comment, :user => @user, :deal => @deal)
-    sign_in(@user)
-
-    delete :destroy, :id => @comment.id, :format => "json"
-
-    assert_equal 200, @response.status
-  end
-  
 end
