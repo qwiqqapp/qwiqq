783e19bd903de4b656a877e3a5694e422e0f6408
Michael Scaria <michaelscaria@yahoo.com>
2012-08-16 13:35:02 -0500

    fix facebook error

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -283,7 +283,7 @@ class Deal < ActiveRecord::Base
     c = ""
     c << "An Awesome Qwiqq #coupon! " if self.coupon?
     c << self.price_as_string if self.price
-    c << " at #{venue_or_location_name}." unless venue_or_location_name.blank?
+    c << " at #{venue_or_location_name}." unless venue_or_location_name.blank? && venue_or_location_name == 'Approximate Location'
     c << " Posted by #{self.user.username}"
     c
   end
