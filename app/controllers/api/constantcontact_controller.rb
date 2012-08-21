e0a6dbe9426ceb090739ce074a609c1ac7b224e7
Michael Scaria <michaelscaria@yahoo.com>
2012-08-21 14:05:34 -0500

    deleted index from cc

--- app/controllers/api/constantcontact_controller.rb
+++ app/controllers/api/constantcontact_controller.rb
@@ -1,8 +1,4 @@
 class Api::ConstantcontactController < Api::ApiController
-  def index
-    @user = current_user
-    Mailer.constant_contact(@user).deliver
-  end
 
   def create
     @user = current_user
