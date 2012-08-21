983c2992673d460ee0011c51416a5446d3645920
Michael Scaria <michaelscaria@yahoo.com>
2012-08-21 17:01:25 -0500

    revert and swtchied food with ae

--- test/factories/categories.rb
+++ test/factories/categories.rb
@@ -1,4 +1,4 @@
 # added rand to avoid duplicates
 Factory.define :category do |f|
-  f.name { %w(food ae beauty sports house travel fashion tech nolocation).shuffle.first + rand(100).to_s } 
+  f.name { %w(ae food beauty sports house travel fashion tech).shuffle.first + rand(100).to_s } 
 end
