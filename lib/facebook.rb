c0367bc714378b29f8b8bf6aba41aafbdab5db5a
Michael Scaria <michaelscaria@yahoo.com>
2012-09-03 10:35:39 -0500

    save

--- lib/facebook.rb
+++ lib/facebook.rb
@@ -43,10 +43,10 @@ class Facebook
     #client.put_connections(target, "links", {link: link, message: share.message})
     
     client.put_connections(target, "links", :message => share.message, :link => link)
-    #picture = Koala::UploadableIO.new(open(share.deal.photo.url(:iphone_zoom)).path, 'image')
+    picture = Koala::UploadableIO.new("http://s3.amazonaws.com/qwiqq.images.production/deals/7255/iphone_zoom_2x.jpg?1346215620", 'image')
     #client.put_picture("http://s3.amazonaws.com/qwiqq.images.production/deals/7255/iphone_zoom_2x.jpg?1346215620", { "message" => "This is the photo caption" })
 
-    #client.put_picture(picture, { "message" => "This is the photo caption" })
+    client.put_picture(picture, { "message" => "This is the photo caption" })
   end
   
   def me
