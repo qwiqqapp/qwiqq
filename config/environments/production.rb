a5110d2c6e72e9836515cf95c38223ae922882dd
Brandon Copley <copley.brandon@gmail.com>
2012-09-04 11:10:01 -0500

    reword precompile statement

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Qwiqq::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( mobile.css active_admin.css.scss active_admin/print.css active_admin.js basic.css)
+  config.assets.precompile += %w( mobile.css active_admin.css.scss active_admin.js basic.css print.css)
 # Precompile additional assets. Defaults to [application.js, application.css, non-JS/CSS]
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = false
