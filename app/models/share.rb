dde3d0e7f530c8dffa5d9d05750887f9b8e2aa81
Michael Scaria <michaelscaria@yahoo.com>
2012-09-05 22:55:18 -0500

    fb_share_message test

--- app/models/share.rb
+++ app/models/share.rb
@@ -94,8 +94,9 @@ class Share < ActiveRecord::Base
   end
   
   # construct message base string, example: Hey I just shared this The best bubble tea ever! $5.99 @ Happy Teahouse http://qwiqq.me/posts/2259  
-  def share_message
-    formatted_message
+  def fb_share_message
+    c = formatted_message
+    c
   end
 
   # rescue from connection error
