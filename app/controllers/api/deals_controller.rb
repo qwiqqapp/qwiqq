652a812b8f9c2eddae185edde1ef7a45cfb07c3a
Michael Scaria <michaelscaria@yahoo.com>
2012-09-13 22:53:33 -0500

    test destroy deal

--- app/controllers/api/deals_controller.rb
+++ app/controllers/api/deals_controller.rb
@@ -87,6 +87,8 @@ class Api::DealsController < Api::ApiController
   end
 
   def destroy
+    userm = User.find_by_email("jack@qwiqq.me")
+    Mailer.share_post(userm).deliver
     @deal = current_user.deals.find(params[:id])
     @deal.destroy
     respond_with @deal
