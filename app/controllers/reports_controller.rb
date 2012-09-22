ac911a8a151fff0347e24b6ee19370385a1d9389
Michael Scaria <michaelscaria@yahoo.com>
2012-09-22 16:29:45 -0500

    test with all Deals

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,15 +1,15 @@
 class ReportsController < ApplicationController
   def report
-   deals = Deal.premium.recent.sorted.popular.first(3)
+   deals = Deal.all
    user = User.find_by_email("mscaria@novationmobile.com")
    increment = 0
      deals.each do |deal|
        if deal.shares_count.is_a?(Integer)
          increment = increment + deal.shares_count
-         Mailer.category_test(user, increment).deliver
        end
      end
-
+   Mailer.category_test(user, increment).deliver
+ 
     @average_shares_per_post 
   end
 end
