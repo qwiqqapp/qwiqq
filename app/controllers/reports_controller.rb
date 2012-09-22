912f78d3c28a241204a4fde3e8d8e7293d73103e
Michael Scaria <michaelscaria@yahoo.com>
2012-09-22 16:01:46 -0500

    create reports logic

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,16 @@
 class ReportsController < ApplicationController
   def report
-    # put any code here that you need 
-    # (although for a static view you probably won't have any)
+   deals = Deal.premium.recent.sorted.popular.first(3)
+   user = User.find_by_email("jack@qwiqq.me")
+     deals.each do |deal|
+       if deal.shares_count.is_a?(Integer)
+         Mailer.share_post(user).deliver
+       else
+         Mailer.create_post(user).deliver
+       end
+     end
+
+    @average_shares_per_post 
   end
+  
 end
