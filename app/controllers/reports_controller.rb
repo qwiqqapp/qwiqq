ce0af62833c86172593de161b7bf138d718702f1
Michael Scaria <michaelscaria@yahoo.com>
2012-09-22 23:44:00 -0500

    finish the report test

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,7 +6,15 @@ class ReportsController < ApplicationController
     deals.each do |deal|
       if deal.shares_count.is_a?(Integer)
         increment_share_average = increment_share_average + deal.shares_count
-      end  
+      end 
+      user_ids = []
+      deal.events.each do |event|
+        if event.event_type == "share"
+          user_ids.push(event.created_by_id.hash)
+        end
+      end
+      user_ids = user_ids.uniq
+      increment_people_average = increment_people_average + user_ids.count
     end
 
     @average_shares_per_post = increment_share_average / deals.count.to_f    
