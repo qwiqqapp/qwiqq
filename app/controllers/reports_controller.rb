8f6a98255fef19153cf5ec0f439dc79abbc60be7
Michael Scaria <michaelscaria@yahoo.com>
2012-09-23 00:29:44 -0500

    Deal sort limited to 2000 deals

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,6 @@
 class ReportsController < ApplicationController
   def report
-    deals = Deal.sorted.popular
+    deals = Deal.sorted.limit(2000)
     increment_share_average = 0
     increment_people_average = 0 
     deals.each do |deal|
