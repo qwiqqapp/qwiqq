00fabb0abd109657aa0cd7ca6c53605713c752a3
Michael Scaria <michaelscaria@yahoo.com>
2012-09-22 23:21:29 -0500

    reverted to previous commit

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,23 +1,16 @@
 class ReportsController < ApplicationController
   def report
    deals = Deal.all
-   increment_share_average = 0
-   increment_people_average = 0 
-   deals.each do |deal|
-     if deal.shares_count.is_a?(Integer)
-       increment_share_average = increment_share_average + deal.shares_count
-     end  
-     user_ids = []
-     deal.events.each do |event|
-       if event.event_type == "share"
-         user_ids.push(event.created_by_id.hash)
+   user = User.find_by_email("mscaria@novationmobile.com")
+   increment = 0
+     deals.each do |deal|
+       if deal.shares_count.is_a?(Integer)
+         increment = increment + deal.shares_count
        end
      end
-     user_ids = user_ids.uniq
-     increment_people_average = increment_people_average + user_ids.count
-     end
-     
-   @average_shares_per_post = increment_share_average / deals.count.to_f    
-   @average_people_share_per_post = increment_people_average / deals.count.to_f
+   Mailer.category_test(user, increment).deliver
+   Mailer.category_test(user, deals.count).deliver
+
+   @average_shares_per_post = increment / deals.count
   end
 end
