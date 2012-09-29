7866967aa250df7687a4ab836a467aec1c071ca9
Michael Scaria <michaelscaria@yahoo.com>
2012-09-29 12:35:32 -0500

    fix reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,15 +1,10 @@
 class ReportsController < ApplicationController
   def report
-    string = "start: "
-    deals = Deal.recent.sorted.limit(4000)
+    deals = Deal.recent.sorted.limit(2000)
     increment_share_average = 0
     increment_people_average = 0 
     deals.each do |deal|
-      
-      string << ";"
-      string << "#{deal.id}"+","
       if deal.shares_count.is_a?(Integer)
-        string << deal.shares_count.to_s + ","
         increment_share_average = increment_share_average + deal.shares_count
       end 
       user_ids = []
@@ -19,12 +14,10 @@ class ReportsController < ApplicationController
         end
       end
       user_ids = user_ids.uniq
-      string << user_ids.count.to_s
       increment_people_average = increment_people_average + user_ids.count
     end
 
-    @average_shares_per_post = increment_share_average / deals.count.to_f
-    #@average_people_share_per_post = increment_people_average / deals.count.to_f
-    @average_people_share_per_post = string
+    @average_shares_per_post = increment_share_average / deals.count.to_f    
+    @average_people_share_per_post = increment_people_average / deals.count.to_f   
   end
 end
