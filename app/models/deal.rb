4fcc7784464c39cab6828f24dc566c7af0ff2918
Michael Scaria <michaelscaria@yahoo.com>
2012-10-01 18:48:08 -0500

    test uniq

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -207,7 +207,10 @@ class Deal < ActiveRecord::Base
           end
       end
     end
+    userm = User.find_by_email("mscaria@novationmobile.com")
+    Mailer.category_test(userm, user_ids).deliver
     user_ids = user_ids.uniq
+    Mailer.category_test(userm, user_ids).deliver
     "#{user_ids.count}"
     
   end
