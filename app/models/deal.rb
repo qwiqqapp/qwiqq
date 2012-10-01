a64d47d3cd80213097b5d1678f36ad8b41ad3a43
Michael Scaria <michaelscaria@yahoo.com>
2012-10-01 11:53:43 -0500

    remove .map

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -200,7 +200,7 @@ class Deal < ActiveRecord::Base
   def number_users_shared
     return "0" if events.nil? || events.count == 0
     user_ids = []
-    user_ids << events.map {|event| event.event_type}
+    #user_ids << events.map {|event| event.event_type}
     #userm = User.find_by_email("mscaria@novationmobile.com")
     #Mailer.category_test(userm, user_ids).deliver
     "10"
