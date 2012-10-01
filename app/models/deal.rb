38d73c60923eb96c42f264141fe388423a9de232
Michael Scaria <michaelscaria@yahoo.com>
2012-09-30 21:26:10 -0500

    test loop

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -200,9 +200,9 @@ class Deal < ActiveRecord::Base
   def number_users_shared
     user_ids = []
     events.each do |event|
-      if event.event_type == "share"
+      #if event.event_type == "share"
         #user_ids.push(event.created_by_id.hash)
-      end
+      #end
     end
     #user_ids = user_ids.uniq
     "10"
