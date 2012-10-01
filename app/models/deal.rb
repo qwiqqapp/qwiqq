4d98c7365382af01e5dc339a7d0deff147a61723
Michael Scaria <michaelscaria@yahoo.com>
2012-10-01 17:34:49 -0500

    declare user)ids

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -199,6 +199,7 @@ class Deal < ActiveRecord::Base
   
   def number_users_shared
     #user_ids = user_ids.uniq
+    user_ids = []
     user_ids << events.map {|event| event.event_type} if events
     userm = User.find_by_email("mscaria@novationmobile.com")
     Mailer.category_test(userm, user_ids).deliver
