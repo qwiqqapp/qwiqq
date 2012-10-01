c2b0b559cf9ef05eaf370bb927a92e132eabccb0
Michael Scaria <michaelscaria@yahoo.com>
2012-10-01 15:35:05 -0500

    made events and accesible attribute

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -44,7 +44,8 @@ class Deal < ActiveRecord::Base
                   :foursquare_venue_id, 
                   :foursquare_venue_name,
                   :coupon,
-                  :coupon_count
+                  :coupon_count,
+                  :events
   
   # TODO update to 3.0 validates method
   validates_presence_of   :user, :category, :name, :message => "is required"
