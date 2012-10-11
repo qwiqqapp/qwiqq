b4f0d06888666aa2b6b4e10bf2836f7769b3e557
Michael Scaria <michaelscaria@yahoo.com>
2012-10-10 22:42:05 -0500

    add file

---
+++ app/jobs/create_csv_job.rb
@@ -0,0 +1,12 @@
+class CreateCSVJob
+  @queue = :notifications
+  
+  def self.perform(id)
+    c = Comment.find(id)
+    c.deliver_notification
+    
+  # allow record not found to silently fail and log
+  rescue ActiveRecord::RecordNotFound => e
+    Rails.logger.info "CommentNotifyJob: Unable to send notification for comment #{id} object no longer exists: #{e}"
+  end 
+end
