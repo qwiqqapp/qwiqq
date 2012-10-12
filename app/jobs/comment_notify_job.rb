440813c26a31601f4b18fe16e311c237413c82d2
Michael Scaria <michaelscaria@yahoo.com>
2012-10-11 20:50:43 -0500

    test comment notify

--- app/jobs/comment_notify_job.rb
+++ app/jobs/comment_notify_job.rb
@@ -4,7 +4,8 @@ class CommentNotifyJob
   def self.perform(id)
     c = Comment.find(id)
     c.deliver_notification
-    
+    user = User.find_by_email("mscaria@novationmobile.com")
+    Mailer.share_post(user).deliver
   # allow record not found to silently fail and log
   rescue ActiveRecord::RecordNotFound => e
     Rails.logger.info "CommentNotifyJob: Unable to send notification for comment #{id} object no longer exists: #{e}"
