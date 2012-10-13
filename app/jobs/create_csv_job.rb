7674c6a00a1bb51ffbaa06294b121a55c894ad64
Michael Scaria <michaelscaria@yahoo.com>
2012-10-12 21:09:04 -0500

    test csv export

--- app/jobs/create_csv_job.rb
+++ app/jobs/create_csv_job.rb
@@ -2,7 +2,7 @@ class CreateCSVJob
   @queue = :notifications
   
   def self.perform(id)
-    #self.csv_export
+    self.csv_export
     puts "MARKED"
 
   # allow record not found to silently fail and log
@@ -16,10 +16,10 @@ class CreateCSVJob
     CSV.open("#{Rails.root.to_s}/tmp/#{@filename}", "wb") do |csv| #creates a tempfile csv
       #csv << ["ID", "Name", "Price", "Created At", "Updated At", "Premium", "Lat", "Lon", "Location Name", "Unique Token", "User Photo", "User Photo2x", "4SQ Venue", "Coupon", "Shares Count", "Number of Users Shared"] #creates the header
       csv << ["ID", "Name"]
-      @deals.each do |deal|   
-        csv << [deal.id.try(:to_s), deal.name]
+      #@deals.each do |deal|   
+        csv << [@deals[0].id.try(:to_s), @deals[0].name]
         #csv << [deal.id.try(:to_s), deal.name, deal.price, deal.created_at, deal.updated_at, deal.premium, deal.lat, deal.lon, deal.location_name, deal.unique_token, deal.user.photo.url(:iphone), deal.user.photo.url(:iphone2x), deal.foursquare_venue_name, deal.coupon, deal.shares_count, deal.number_users_shared] #create new line for each item in collection
-      end
+      #end
     end
 
     #self.update_attribute(:csv_report, File.open("#{Rails.root.to_s}/tmp/#{@filename}"))
