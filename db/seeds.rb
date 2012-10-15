d1c042f67618aa9b52c71bb2dcf3e1667c088e6b
Brandon Copley <copley.brandon@gmail.com>
2012-10-15 10:58:41 -0500

    update admin passwords

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,18 +3,15 @@ puts '>> creating new categories'
 Rake::Task['category:update'].invoke
 
 puts '>> creating admin accounts'
-AdminUser.create!(:email => 'adam@gastownlabs.com',     :password => 'texasbbq', :password_confirmation => 'texasbbq')
-AdminUser.create!(:email => 'brian@gastownlabs.com',    :password => 'texasbbq', :password_confirmation => 'texasbbq')
-AdminUser.create!(:email => 'eoin@gastownlabs.com',     :password => 'texasbbq', :password_confirmation => 'texasbbq')
-AdminUser.create!(:email => 'kristina@gastownlabs.com', :password => 'texasbbq', :password_confirmation => 'texasbbq')
+AdminUser.create!(:email => 'adam@gastownlabs.com',       :password => 'texasbbq', :password_confirmation => 'texasbbq')
+AdminUser.create!(:email => 'brian@gastownlabs.com',      :password => 'texasbbq', :password_confirmation => 'texasbbq')
+AdminUser.create!(:email => 'eoin@gastownlabs.com',       :password => 'texasbbq', :password_confirmation => 'texasbbq')
+AdminUser.create!(:email => 'kristina@gastownlabs.com',   :password => 'texasbbq', :password_confirmation => 'texasbbq')
 
-AdminUser.create!(:email => 'john@qwiqq.me',            :password => 'texasbbq', :password_confirmation => 'texasbbq')
-AdminUser.create!(:email => 'jack@qwiqq.me',            :password => 'texasbbq', :password_confirmation => 'texasbbq')
+AdminUser.create!(:email => 'john@qwiqq.me',              :password => 'texasbbq', :password_confirmation => 'texasbbq')
+AdminUser.create!(:email => 'jack@qwiqq.me',              :password => 'texasbbq', :password_confirmation => 'texasbbq')
 
-
-
-
-
+AdminUser.create!(:email => 'brandon@novationmobile.com', :password => 'texasbbq', :password_confirmation => 'texasbbq')
 
 # 
 
