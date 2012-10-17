d68fb30fec7a58e455d89deb4f4db70c9d3ce9f1
Michael Scaria <michaelscaria@yahoo.com>
2012-10-16 22:41:39 -0500

    set most_shared to return premium deals

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -65,7 +65,7 @@ class Deal < ActiveRecord::Base
   scope :sorted, :order => "created_at desc"
   scope :popular, order("likes_count desc, comments_count desc")
   scope :coupon, where(:coupon => true)
-  scope :most_shared, order("number_users_shared desc")
+  scope :most_shared, where(:premium => true)
   
   # all images are cropped
   # see initializers/auto_orient.rb for new processor
