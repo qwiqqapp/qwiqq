4328111914cee637cdfdbe4f780f117fc978ee39
Michael Scaria <michaelscaria@yahoo.com>
2012-11-05 22:57:38 -0600

    revert

--- app/admin/deals.rb
+++ app/admin/deals.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register Deal, :namespace => "Posts" do
+ActiveAdmin.register Deal do
   #ActiveAdmin.register Deal, :as => "Posts" do
   menu :label => "Posts"
   actions :index, :show, :edit, :update, :destroy
