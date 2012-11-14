5e1c30857b1e00cfb8a331959755c42d28a40db2
Michael Scaria <michaelscaria@yahoo.com>
2012-11-13 20:45:35 -0600

    only add paypal fields if enabled on deal

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -148,11 +148,10 @@ class Deal < ActiveRecord::Base
       :number_users_shared    => number_users_shared,
     }
     
-    #if :for_sale_on_paypal
+    if :for_sale_on_paypal
       json[:num_left_for_sale] = num_left_for_sale
       json[:num_for_sale]           = num_for_sale
-      json[:for_sale_on_paypal]      = for_sale_on_paypal
-    #end
+    end
     # add 'liked' for the current user if requested
     current_user = options[:current_user]
     json[:liked] = current_user.liked_deals.include?(self) if current_user
