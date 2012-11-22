44c85e5ef35626b88f366726596e82198b57e592
Michael Scaria <michaelscaria@yahoo.com>
2012-11-22 17:56:06 -0600

    add pagination to following

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -113,7 +113,9 @@ class Api::UsersController < Api::ApiController
   def following
     requested_user
     @following = @user.following.sorted
-    respond_with @following.as_json(:current_user => current_user)
+    #respond_with @following.as_json(:current_user => current_user)
+    respond_with(paginate(@following), :current_user => current_user)
+
   end
 
   def friends
