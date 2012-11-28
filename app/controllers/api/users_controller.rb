cc07483b408eb3401b21e72e6f4e6e711da652d4
Michael Scaria <michaelscaria@yahoo.com>
2012-11-27 22:41:43 -0600

    use default page instead of result.count

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -116,6 +116,8 @@ class Api::UsersController < Api::ApiController
     #result = @following.page(params[:page])
     #puts "Count following:#{@following.count} result count:#{result.count}"
     #number = (@following.count.to_f / result.count.to_f).ceil.to_s
+    string = (collection.count / result.default_per_page.to_f).ceil.to_s
+    puts "The Total Pages for Following is #{string}"
     #response.headers["X-Pages"] = number
     #puts "Total number of queries needed #{number}"
     respond_with @following.as_json(:current_user => current_user)
