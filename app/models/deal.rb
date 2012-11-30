546024c3fde560608a88186cd65bbdd42def555c
Brandon Copley <copley.brandon@gmail.com>
2012-11-30 16:09:05 -0600

    add for_sales_on_paypal to json

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -149,12 +149,13 @@ class Deal < ActiveRecord::Base
     }
     
     if :for_sale_on_paypal
+      json[:for_sale_on_paypal] = for_sale_on_paypal;
       json[:num_left_for_sale]  = num_left_for_sale
       json[:num_for_sale]       = num_for_sale
       json[:currency]           = currency
       json[:paypal_email]       = paypal_email
-
     end
+    
     # add 'liked' for the current user if requested
     current_user = options[:current_user]
     json[:liked] = current_user.liked_deals.include?(self) if current_user
