7f946155637af03b8273f96803ae4af3bc35d416
Michael Scaria <michaelscaria@yahoo.com>
2012-12-02 18:54:10 -0600

    about to merge

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -117,7 +117,8 @@ class Api::UsersController < Api::ApiController
     result = @following.page(params[:page])
     #puts "Count following:#{@following.count} result count:#{result.count}"
     string = (@following.count / result.default_per_page.to_f).ceil.to_s
-    #response.headers["X-Pages"] = string
+    #create custom x- response header data to transfer the number of pages
+    response.headers["X-Total-Pages"] = string
     puts "Total number of queries needed #{string}"
     respond_with @following.as_json(:current_user => current_user)
   end
