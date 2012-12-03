ae6b81a335e8372a99dcc1db2b7f0b9d596bb036
Michael Scaria <michaelscaria@yahoo.com>
2012-12-02 19:50:22 -0600

    show paypal values regardless of shown

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -146,14 +146,19 @@ class Deal < ActiveRecord::Base
       :share_count    => shares_count,
       
       :number_users_shared    => number_users_shared,
+      :for_sale_on_paypal = for_sale_on_paypal,
+      :num_left_for_sale  = num_left_for_sale,
+      :num_for_sale       = num_for_sale,
+      :currency           = currency,
+      :paypal_email       = paypal_email,
     }
     
     #if :for_sale_on_paypal
-      json[:for_sale_on_paypal] = for_sale_on_paypal;
-      json[:num_left_for_sale]  = num_left_for_sale
-      json[:num_for_sale]       = num_for_sale
-      json[:currency]           = currency
-      json[:paypal_email]       = paypal_email
+      #json[:for_sale_on_paypal] = for_sale_on_paypal,
+      #json[:num_left_for_sale]  = num_left_for_sale,
+      #json[:num_for_sale]       = num_for_sale,
+      #json[:currency]           = currency,
+      #json[:paypal_email]       = paypal_email,
     #end
     
     # add 'liked' for the current user if requested
