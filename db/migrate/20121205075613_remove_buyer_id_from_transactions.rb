5b4f347d4f3e7c6a805851a99d106c20f43a5c92
Michael Scaria <michaelscaria@yahoo.com>
2012-12-05 19:52:01 -0600

    delete integer in remove column

--- db/migrate/20121205075613_remove_buyer_id_from_transactions.rb
+++ db/migrate/20121205075613_remove_buyer_id_from_transactions.rb
@@ -1,6 +1,6 @@
 class RemoveBuyerIdFromTransactions < ActiveRecord::Migration
   def up
-    remove_column :transactions, :buyer_id, :integer
+    remove_column :transactions, :buyer_id
   end
     
   def down
