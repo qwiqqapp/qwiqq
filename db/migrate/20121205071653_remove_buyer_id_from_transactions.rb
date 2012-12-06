00014f2960278f896b6e14d217ed953e1a3d62a5
Michael Scaria <michaelscaria@yahoo.com>
2012-12-05 21:25:00 -0600

    added migration to add transaction count remove old remove

--- db/migrate/20121205071653_remove_buyer_id_from_transactions.rb
+++
@@ -1,9 +0,0 @@
-class RemoveBuyerIdFromTransactions < ActiveRecord::Migration
-  def up
-    remove_column :transactions, :buyer_id
-  end
-    
-  def down
-    add_column :transactions, :buyer_id, :integer
-  end
-end
