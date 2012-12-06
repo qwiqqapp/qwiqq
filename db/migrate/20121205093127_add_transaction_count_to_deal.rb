db7e5961c07e96563d7de333b1bd4f4f1777ac2a
Michael Scaria <michaelscaria@yahoo.com>
2012-12-05 21:08:20 -0600

    added migration to add transaction count

---
+++ db/migrate/20121205093127_add_transaction_count_to_deal.rb
@@ -0,0 +1,11 @@
+class AddTransactionCountToDeals < ActiveRecord::Migration
+    
+  def self.up
+    add_column :deals, :transaction_count, :integer, :default => 0
+  end
+
+  def self.down
+    remove_column :deals, :transaction_count
+  end
+
+end
