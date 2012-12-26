abf08fb88e770ed11d8643bde1ffaf8612b07011
Michael Scaria <michaelscaria@yahoo.com>
2012-12-26 17:04:46 -0600

    fix application error in coupons

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -2,7 +2,7 @@ class CouponsController < ApplicationController
   layout false
   before_filter :find_deal
   attr_reader :deal
-  helper_method :deal, :redeemed?, test_paypal
+  helper_method :deal, :redeemed?, :test_paypal
 
   def show
     @redeemed = deal.redeem_coupon!
