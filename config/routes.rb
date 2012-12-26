d1394274beaade015de9b6342f739b0ccb6a1466
Michael Scaria <michaelscaria@yahoo.com>
2012-12-26 15:29:37 -0600

    added new routes to get to deal test_paypal method remove javascript

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Qwiqq::Application.routes.draw do
   resources :posts, :only => [:index, :show], :as => "deals", :controller => "deals" do
     get :nearby, :on => :collection
     resource :coupon, :only => [:show]
+    match "buy",    :to => "deal#test_paypal"
   end
   resources :users, :only => [:show]
   
