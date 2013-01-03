b70cedd0e608769f5e3da8ce816728e9096591da
Michael Scaria <michaelscaria@yahoo.com>
2013-01-03 14:28:21 -0600

    try to fix sorting problem  for admin transactions

--- app/admin/transactions.rb
+++ app/admin/transactions.rb
@@ -16,27 +16,15 @@ ActiveAdmin.register Transaction do
   end
     
   index do
-    column("ID"){|transaction| transaction.id.try(:to_s)}
-    column("Paypal Transaction ID"){ |transaction| transaction.paypal_transaction_id }
-    column("Buyer ID"){ |transaction| transaction.user.id }
-    column("Buyer Username"){ |transaction| transaction.user.username }
-    column("Seller ID"){ |transaction| transaction.deal.user.id }
-    column("Seller Username"){ |transaction| transaction.deal.user.username }
-    column("Deal ID") {|transaction| transaction.deal.id.try(:to_s)}
-    default_actions
-  end
-   
-  
-  index do
-    column("Buyer", :sortable => :name) do |transaction|  
+    column("Buyer", :sortable => :user.name) do |transaction|  
       link_to(transaction.user.name, [ :admin, transaction.user ]) if transaction.user
     end
     
-    column("Seller", :sortable => :name) do |transaction|  
+    column("Seller", :sortable => :deal.user.name) do |transaction|  
       link_to(transaction.deal.user.name, [ :admin, transaction.deal.user ]) if transaction.deal.user
     end
     
-    column("Deal", :sortable => :name) do |transaction|  
+    column("Deal", :sortable => :deal.name) do |transaction|  
       link_to(transaction.deal.name, [ :admin, transaction.deal ]) if transaction.deal
     end
 
