6b010894ca78c2547937f184a770e60bde353f8c
Michael Scaria <michaelscaria@yahoo.com>
2013-01-04 17:30:34 -0600

    add logs

--- app/helpers/adaptive_pay.rb
+++ app/helpers/adaptive_pay.rb
@@ -14,8 +14,8 @@ module AdaptivePay
       puts "THE IPN DETERMINE IF SANDBOX:#{@params[:test_ipn]}"
       
       string = "sandbox." if @params[:sandbox] == true
-      puts "TEST STRING'#{string.to_s}'"
-      url = 'https://www.' + string.to_s + 'paypal.com/cgi-bin/webscr?cmd=_notify-validate'
+      puts "TEST STRING'#{string}'"
+      url = 'https://www.' + (string || "") + 'paypal.com/cgi-bin/webscr?cmd=_notify-validate'
       puts "TEST URL'#{url}'"
       uri = URI.parse(url)
 
