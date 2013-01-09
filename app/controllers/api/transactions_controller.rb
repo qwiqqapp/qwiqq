a20ef8fbb09c54295057e60403150c84d01c71e8
Brandon Copley <copley.brandon@gmail.com>
2013-01-09 15:35:09 -0600

    using real transaction column names...

--- app/controllers/api/transactions_controller.rb
+++ app/controllers/api/transactions_controller.rb
@@ -57,7 +57,7 @@ class Api::TransactionsController < Api::ApiController
           theID = firstReceiver['.id']
           puts "RECEVIER ID:#{theID}"
           
-          if Transaction.exists?(:transaction_id => theID)
+          if Transaction.exists?(:paypal_transaction_id => theID)
             puts 'the transaction already exists...therefore we dont send an email'
           else
             Mailer.deal_purchased(@transaction.user, @deal, @transaction).deliver
