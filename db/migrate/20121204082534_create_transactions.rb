a5ab7c795d29b0038036c22adf64fc7e1995fcb4
Brandon Copley <copley.brandon@gmail.com>
2013-01-09 16:08:53 -0600

    save

--- db/migrate/20121204082534_create_transactions.rb
+++ db/migrate/20121204082534_create_transactions.rb
@@ -1,7 +1,7 @@
 class CreateTransactions < ActiveRecord::Migration
   def self.up
     create_table :transactions do |t|
-      t.integer :paypal_transaction_id
+      t.string :paypal_transaction_id
       t.integer :deal_id
       t.integer :buyer_id
       t.integer :user_id
--- db/migrate/20130109152336_change_type_of_paypal_transaction_id_to_string.rb
+++
@@ -1,9 +0,0 @@
-class ChangeTypeOfPayPalTransactionIdToString < ActiveRecord::Migration
-  def self.up
-    change_column :transaction, :paypal_transaction_id, :string
-  end
-
-  def self.down
-    change_column :transaction, :paypal_transaction_id, :integer
-  end
-end
