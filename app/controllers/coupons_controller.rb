74b11d18d61b53abda29b4960289e6fd7f069203
Michael Scaria <michaelscaria@yahoo.com>
2013-01-13 23:10:48 -0600

    add test paypal request

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,3 +1,5 @@
+require 'adaptive_pay/interface'
+
 class CouponsController < ApplicationController
   layout false
   before_filter :find_deal
@@ -14,7 +16,32 @@ class CouponsController < ApplicationController
   
   def test_ajax
     puts "TESTED AJAX"
-    redirect_to root_url and return
+    interface = AdaptivePay::Interface.new
+    response = interface.request_payment do |request|
+      request.currency_code = "USD"
+
+      request.cancel_url = "http://example.com/cancelled_payment"        # this is where the user will be redirected should he cancel the payment
+      request.return_url = "http://example.com/completed_payment"       # and here should the payment be succesful
+      request.ipn_notification_url = "http://example.com/ipn_callback"      
+
+      request.add_recipient :email => "copley.brandon@gmail.com",
+                          :amount => 100,
+                         :primary => false
+
+      request.add_recipient :email => "michaelscaria@yahoo.com",
+                          :amount => 10,
+                          :primary => true
+    end
+    
+    if response.created?
+      # the payment has been setup successfully, now the user will need to be redirected to the Paypal site:
+      redirect_to response.payment_page_url
+    else
+      # the payment could not be setup, most likely because of a missing parameter or validation error
+      # the array of errors reported by the service can be retrieved using:
+      response.errors
+    end
+    
   end
  
 
