67a11a91d2a3cb23f0af235a080fa1edfe54ae82
Michael Scaria <michaelscaria@yahoo.com>
2013-01-17 22:06:22 -0600

    try adding some http data test

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -14,6 +14,25 @@ class CouponsController < ApplicationController
   
   def paypal_test
     puts "AJAX WORKED"
+    http = Net::HTTP.new('https://svcs.paypal.com', 80)
+    path = '/AdaptivePayments/Pay'
+
+    data = '{\"actionType\":\"PAY\", \"currencyCode\":\"USD\", \"receiverList\":{\"receiver\":[{\"amount\":\"1.00\",\"email\":\"rec1_1312486368_biz@gmail.com\"}]}, \"returnUrl\":\"http://www.google.com\", \"cancelUrl\":\"http://www.facebook.com\", \"requestEnvelope\":{\"errorLanguage\":\"en_US\", \"detailLevel\":\"ReturnAll\"}}'
+    headers = {
+      'Cookie' => cookie,
+      'Content-Type' => 'application/x-www-form-urlencoded',
+      "X-PAYPAL-SECURITY-USERID" => "caller_1312486258_biz_api1.gmail.com",
+      "X-PAYPAL-SECURITY-PASSWORD" => "1312486294",
+      "X-PAYPAL-SECURITY-SIGNATURE" => "AbtI7HV1xB428VygBUcIhARzxch4AL65.T18CTeylixNNxDZUu0iO87e",
+      "X-PAYPAL-REQUEST-DATA-FORMAT" => "JSON",
+      "X-PAYPAL-RESPONSE-DATA-FORMAT" => "JSON",
+      "X-PAYPAL-APPLICATION-ID" => "APP-80W284485P519543T"
+      
+    }
+
+    resp, data = http.post(path, data, headers)
+    puts "PAYPAL SUCCESS"
+
   end
 
 private
