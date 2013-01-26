fba5eabc1c9cf00890a9faf1f981a4723adf946a
Michael Scaria <michaelscaria@yahoo.com>
2013-01-26 17:14:41 -0600

    test metioned users

--- app/models/user_event.rb
+++ app/models/user_event.rb
@@ -77,7 +77,8 @@ class UserEvent < ActiveRecord::Base
   end
   
   def mentioned_users
-    body.scan(/@([\w-]+)/).map {|match| 
+    
+    metadata[:body].scan(/@([\w-]+)/).map {|match| 
     puts "EVENT MENTIONED:#{match} @ ZERO:#{match[0]}"
     User.find_by_username(match[0])}.compact
   end
