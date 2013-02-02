f91a44c70c481cac26d989f4f57ba5a785327ba4
Michael Scaria <michaelscaria@yahoo.com>
2013-02-02 15:58:48 -0600

    choose recent deals that are premium to display

--- app/controllers/api/explore_controller.rb
+++ app/controllers/api/explore_controller.rb
@@ -58,7 +58,7 @@ class Api::ExploreController < Api::ApiController
   
   def popular
     puts "POPULAR FOUND TEST"
-    @deals = Deal.premium.recent_explore.first(18)
+    @deals = Deal.premium.recent.first(18)
     puts @deals
     options = { :minimal => true }
     options[:current_user] = current_user if current_user
