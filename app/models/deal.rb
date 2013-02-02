a6ffaf9a8b58949346c0e437e947ed62f95e554f
Michael Scaria <michaelscaria@yahoo.com>
2013-02-02 16:56:23 -0600

    try new self.search

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -314,7 +314,7 @@ class Deal < ActiveRecord::Base
 
   def self.search(search)
     search_condition = "%" + search + "%"
-    find(:all, :conditions => ['title LIKE ? OR description LIKE ?', search_condition, search_condition])
+    find(:all, :conditions => ['name LIKE ?', search_condition])
   end
 
   def locate_via_foursquare!
