44809ba9e2093aaa9a62b261ffdc3e52b5034751
Brandon Copley <copley.brandon@gmail.com>
2013-02-08 12:05:18 -0600

    add email test template place

--- app/controllers/testing_controller.rb
+++ app/controllers/testing_controller.rb
@@ -2,13 +2,16 @@ class TestingController < ApplicationController
   def deal_purchased
     @deal ||= Deal.find(11047)
     
-    @transaction = Transaction.create(:deal => @deal, :paypal_transaction_id => 11234);
+    @transaction = Transaction.create(:deal => @deal, :paypal_transaction_id => rand(10000000));
+    
+    @transaction.user = User.find(12998)
+    @transaction.email = @transaction.user.email;
     
     #@transaction = Transaction.find()
-    mail :to => target_email, 
-         :tag => "voucher",
-         :subject => "You just bought something on Qwiqq!",
-         :template_name => 'deal_purchased' 
+    #mail :to => 'copley.brandon@gmail.com', 
+    #     :tag => "voucher",
+    #     :subject => "You just bought something on Qwiqq!",
+    #     :template_name => 'deal_purchased' 
     render layout: 'deal_purchased'
   end
 end
