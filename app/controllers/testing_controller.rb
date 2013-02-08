449f46e8cdd90a4d6b6c7e969043064a5aa2e199
Brandon Copley <copley.brandon@gmail.com>
2013-02-08 13:56:41 -0600

    add transactions, etc.

--- app/controllers/testing_controller.rb
+++ app/controllers/testing_controller.rb
@@ -16,7 +16,7 @@ class TestingController < ApplicationController
   end
   
   def deal_sold
-    @deal ||= Deal.find(11047)
+    @deal ||= Deal.find(11049)
     
     @transaction = Transaction.create(:deal => @deal, :paypal_transaction_id => rand(10000000));
     
