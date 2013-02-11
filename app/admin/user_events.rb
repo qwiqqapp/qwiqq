cc553c63bc9a0682b22a7279450752e4fbc79f4f
Michael Scaria <michaelscaria@yahoo.com>
2013-02-11 17:42:05 -0600

    add user events to admin to check stuff

---
+++ app/admin/user_events.rb
@@ -0,0 +1,19 @@
+ActiveAdmin.register UserEvents do
+
+  
+  index do
+    id_column   
+    column("User", :sortable => :name) do |event|  
+      link_to(event.user.name, [ :admin, event.user ]) if event.user
+    end
+    
+    column("Deal", :sortable => :name) do |event|  
+      link_to(event.deal.name, [ :admin, event.deal ]) if event.deal
+    end  
+    column :created_at
+    column :updated_at
+    
+    default_actions
+  end
+  
+end
