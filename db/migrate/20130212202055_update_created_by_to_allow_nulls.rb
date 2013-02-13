de4fc59c1193c1ebbc43d3745610f6374cefe5d1
Michael Scaria <michaelscaria@yahoo.com>
2013-02-12 20:17:07 -0600

    removed references and used integers

--- db/migrate/20130212202055_update_created_by_to_allow_nulls.rb
+++ db/migrate/20130212202055_update_created_by_to_allow_nulls.rb
@@ -1,7 +1,7 @@
 class UpdateCreatedByToAllowNulls < ActiveRecord::Migration
   def self.up
-    change_column :user_events, :user, :references, :null => true
-    change_column :user_events, :created_by, :references, :null => true
+    change_column :user_events, :user, :integer, :null => true
+    change_column :user_events, :created_by, :integer, :null => true
     change_column :user_events, :created_by_photo, :string, :null => true
     change_column :user_events, :created_by_photo_2x, :string, :null => true
     change_column :user_events, :created_by_username, :string, :null => true
@@ -9,8 +9,8 @@ class UpdateCreatedByToAllowNulls < ActiveRecord::Migration
 end
 
   def self.down
-    change_column :user_events, :user, :references, :null => false
-    change_column :user_events, :created_by, :references, :null => false
+    change_column :user_events, :user, :integer, :null => false
+    change_column :user_events, :created_by, :integer, :null => false
     change_column :user_events, :created_by_photo, :string, :null => false
     change_column :user_events, :created_by_photo_2x, :string, :null => false
     change_column :user_events, :created_by_username, :string, :null => false
