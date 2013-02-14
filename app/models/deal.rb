3e10172727bc1633a1d87fe9f0c5a86b7c746eed
Brandon Copley <copley.brandon@gmail.com>
2013-02-14 16:30:44 -0600

    update qwiqq logo on merchant site

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -199,7 +199,6 @@ class Deal < ActiveRecord::Base
     if self.price > 0
       if self.currency
         if !self.currency.empty?
-          puts "money sign is curreny #{number_to_currency(price.to_f / 100, :unit => self.currency).sub( "USD", "$" )}" 
           number_to_currency(price.to_f / 100, :unit => self.currency).sub( "USD", "$" )
         else
           number_to_currency(price.to_f / 100)
