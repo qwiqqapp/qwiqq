8a982a0fa7c9b0018ad3ebf8982c68b20f14a275
Michael Scaria <michaelscaria@yahoo.com>
2013-02-14 23:18:07 -0600

    use brandon's code

--- app/controllers/api/constantcontact_controller.rb
+++ app/controllers/api/constantcontact_controller.rb
@@ -9,6 +9,19 @@ class Api::ConstantcontactController < Api::ApiController
   
   def email
     puts "create email to send"
-    render layout: 'share_post'
+    #render layout: 'share_post'
+        @deal ||= Deal.find(11049)
+    
+    @transaction = Transaction.create(:deal => @deal, :paypal_transaction_id => rand(10000000));
+    
+    @transaction.user = User.find(12998)
+    @transaction.email = @transaction.user.email;
+    
+    #@transaction = Transaction.find()
+    #mail :to => 'copley.brandon@gmail.com', 
+    #     :tag => "voucher",
+    #     :subject => "You just bought something on Qwiqq!",
+    #     :template_name => 'deal_purchased' 
+    render layout: 'deal_sold'
   end
 end
