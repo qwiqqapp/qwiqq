65d354a83222b48588d17a98317b61d69aeec6fa
Michael Scaria <michaelscaria@yahoo.com>
2013-02-21 22:21:22 -0600

    save

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -456,8 +456,8 @@ class Deal < ActiveRecord::Base
   def meta_content
     c = ""
     c << self.name
-    c << "#shopsmall"
-    c << "BUY NOW" if self.for_sale_on_paypal && self.num_left_for_sale > 0
+    c << " #shopsmall"
+    c << " BUY NOW" if self.for_sale_on_paypal && self.num_left_for_sale > 0
     c << self.price_as_string if self.price
     c << Rails.application.routes.url_helpers.deal_url(self, :host => "qwiqq.me")
     c
