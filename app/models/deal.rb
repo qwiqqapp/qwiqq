e103a17752bf9c31a778efb5f33dc96c7ddc12a9
Michael Scaria <michaelscaria@yahoo.com>
2013-02-23 00:11:06 -0600

    revert meta and share messages

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -223,13 +223,11 @@ class Deal < ActiveRecord::Base
   # construct message base string, example: The best bubble tea ever! $5.99 @ Happy Teahouse http://qwiqq.me/posts/2259  DEPRECATED
   #new message - <post name> #shopsmall BUY NOW(if paypal) <price> <url link>
   def share_message
-    meta = "#shopsmall"
-    meta << "BUY NOW" if self.for_sale_on_paypal && self.num_left_for_sale > 0
-    meta << self.price_as_string if self.price
+    meta = price_as_string || ""
+    meta << " @ #{foursquare_venue_name}" if foursquare_venue_name
     meta << " #{Rails.application.routes.url_helpers.deal_url(self, :host => "qwiqq.me")}"
-    t = "#{name.truncate(138 - meta.size)} #{meta}"
-    puts "SHARE_MESSAGE:#{t}"
-    t
+    
+    "#{name.truncate(138 - meta.size)} #{meta}"
   end
 
 
@@ -455,12 +453,11 @@ class Deal < ActiveRecord::Base
   end
   
   def meta_content
-    c = self.name
-    c << " #shopsmall"
-    c << " BUY NOW" if self.for_sale_on_paypal && self.num_left_for_sale > 0
-    c << " #{self.price_as_string}" if self.price
-    c << " #{Rails.application.routes.url_helpers.deal_url(self, :host => "qwiqq.me")}"
-    puts "META_CONTENT:#{c}"
+    c = ""
+    c << "An Awesome Qwiqq #coupon! " if self.coupon?
+    c << self.price_as_string if self.price
+    c << " at #{venue_or_location_name}." unless venue_or_location_name.blank?
+    c << " Posted by @#{self.user.username}"
     c
   end
 
