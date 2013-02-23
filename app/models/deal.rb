8e822a8a4daa9ba94c2541d4f7681a5c0cf54eb6
Michael Scaria <michaelscaria@yahoo.com>
2013-02-22 23:51:33 -0600

    added logs for meta content

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -459,6 +459,7 @@ class Deal < ActiveRecord::Base
     c << " BUY NOW" if self.for_sale_on_paypal && self.num_left_for_sale > 0
     c << " #{self.price_as_string}" if self.price
     c << " #{Rails.application.routes.url_helpers.deal_url(self, :host => "qwiqq.me")}"
+    puts "META_CONTENT:#{c}"
     c
   end
 
