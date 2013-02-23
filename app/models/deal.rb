067bc7a61d382f43ad0c8a6a06c345c8e9b05166
Michael Scaria <michaelscaria@yahoo.com>
2013-02-22 23:46:08 -0600

    try to update meta data

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -454,12 +454,11 @@ class Deal < ActiveRecord::Base
   end
   
   def meta_content
-    c = ""
-    c << self.name
+    c = self.name
     c << " #shopsmall"
     c << " BUY NOW" if self.for_sale_on_paypal && self.num_left_for_sale > 0
-    c << self.price_as_string if self.price
-    c << Rails.application.routes.url_helpers.deal_url(self, :host => "qwiqq.me")
+    c << " #{self.price_as_string}" if self.price
+    c << " #{Rails.application.routes.url_helpers.deal_url(self, :host => "qwiqq.me")}"
     c
   end
 
