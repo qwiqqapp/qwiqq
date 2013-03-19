444a3f321d5abe9bcced5766253c7598fd069216
Michael Scaria <michaelscaria@yahoo.com>
2013-03-18 20:45:09 -0500

    test share message

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -233,9 +233,9 @@ class Deal < ActiveRecord::Base
     end
     meta << self.price_as_string if self.price
     meta << " #{Rails.application.routes.url_helpers.deal_url(self, :host => "qwiqq.me")}"
-    puts "2TEST SHARE MESSAGE BEFORE TRUNCATE:#{meta}"
-    t = "#{name.truncate(138 - meta.size)} #{meta}"
-    puts "2TEST SHARE MESSAGE:#{t}"
+    puts "3TEST SHARE MESSAGE BEFORE TRUNCATE:#{meta} WITH NAME:#{self.name}"
+    t = "#{self.name.truncate(138 - meta.size)} #{meta}"
+    puts "3TEST SHARE MESSAGE:#{t}"
     t
   end
   
