334a33ecf30ca6b701b7174f3a3850cdd3c81713
Brandon Copley <copley.brandon@gmail.com>
2013-04-05 16:39:34 -0500

    update json files

--- app/models/user.rb
+++ app/models/user.rb
@@ -259,12 +259,12 @@ class User < ActiveRecord::Base
         json[:deal_count] = deals_count
         json[:comment_count] = comments_count
         json[:transaction_count] = transactions_count
-        json[:deals] = options[:deals]  ? deals.sorted.limit(20).to_json(:minimal=>true) : nil
+        json[:deals] = options[:deals]  ? deals.sorted.limit(20).as_json(:minimal=>true) : nil
         #is this really necessary?
         #json[:liked_deals] = options[:deals]    ? liked_deals.sorted.limit(6) : nil
-        json[:comments] = options[:comments] ? comments.limit(6).to_json(:minimal=>true) : nil
+        json[:comments] = options[:comments] ? comments.limit(6).as_json(:minimal=>true) : nil
         
-        json[:events] = options[:events]   ? events.limit(20) : nil
+        json[:events] = options[:events]   ? events.limit(20).as_json(:minimal=>true) : nil
         
       end
     
