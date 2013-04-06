219fb29b5e24a03d4c5ba1201c213477eab55874
Brandon Copley <copley.brandon@gmail.com>
2013-04-06 08:34:57 -0500

    minify following data

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -126,7 +126,7 @@ class Api::UsersController < Api::ApiController
   
   def following
     requested_user
-    @following = @user.following.sorted
+    @following = @user.following.sorted#.take(5000) #<- this would limit the number returned if we see issues in the future
     
     respond_with @following.as_json(:current_user => current_user,:minimal=>true)
   end
