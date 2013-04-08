3973eaacecd403921fb50c3757bae6d44f7d27a9
Michael Scaria <michaelscaria@yahoo.com>
2013-04-08 17:57:36 -0500

    new migrations

--- db/migrate/20110823232108_add_likes_count_and_comment_count_to_user.rb
+++ db/migrate/20110823232108_add_likes_count_and_comment_count_to_user.rb
@@ -1,16 +1,16 @@
 class AddLikesCountAndCommentCountToUser < ActiveRecord::Migration
   def self.up
-    add_column :users, :likes_count, :integer, :default => 0
-    add_column :users, :comments_count, :integer, :default => 0
-    add_column :users, :deals_count, :integer, :default => 0
-    ActiveRecord::Base.connection.execute("update users set comments_count = (select count(id) from comments where comments.user_id = users.id)")
-    ActiveRecord::Base.connection.execute("update users set likes_count = (select count(id) from likes where likes.user_id = users.id)")
-    ActiveRecord::Base.connection.execute("update users set deals_count = (select count(id) from deals where deals.user_id = users.id)")
+    # add_column :users, :likes_count, :integer, :default => 0
+    #add_column :users, :comments_count, :integer, :default => 0
+    #add_column :users, :deals_count, :integer, :default => 0
+    #ActiveRecord::Base.connection.execute("update users set comments_count = (select count(id) from comments where comments.user_id = users.id)")
+    #ActiveRecord::Base.connection.execute("update users set likes_count = (select count(id) from likes where likes.user_id = users.id)")
+    #ActiveRecord::Base.connection.execute("update users set deals_count = (select count(id) from deals where deals.user_id = users.id)")
   end
 
   def self.down
-    remove_column :users, :likes_count
-    remove_column :users, :comments_count
-    remove_column :users, :deals_count
+    #remove_column :users, :likes_count
+    #remove_column :users, :comments_count
+    #remove_column :users, :deals_count
   end
 end
