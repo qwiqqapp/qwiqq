0886d8eabe2953c35b11d991488231a214e0c4e0
Brandon Copley <copley.brandon@gmail.com>
2013-04-09 11:47:51 -0500

    something interesting

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -5,6 +5,7 @@ require "resque/failure/airbrake"
 
 Resque::Failure::Multiple.classes = [ Resque::Failure::Redis, Resque::Failure::Airbrake ]
 Resque::Failure.backend = Resque::Failure::Multiple
+Resque.before_fork = Proc.new { ActiveRecord::Base.establish_connection }
 Resque.after_fork do |worker|
   ActiveRecord::Base.establish_connection
 end
