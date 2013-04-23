02b957f2cf183135a8093899024f7b8676b5dc8e
Brandon Copley <copley.brandon@gmail.com>
2013-04-23 10:53:54 -0500

    currency symbol was not working

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -215,14 +215,14 @@ class Deal < ActiveRecord::Base
       if self.currency
         if !self.currency.empty?
           currencySymbol = number_to_currency(price.to_f / 100, :unit => self.currency)
-          currencySymbol.sub("AUD","AUD $").sub( "BRL", "R$" ).sub( "CAD", "CAD $" )
-          currencySymbol.sub("CZK","CZK").sub( "DKK", "DKK" ).sub( "EUR", "€" )
-          currencySymbol.sub("HKD","HK$").sub( "HUF", "HUF" ).sub( "ILS", "₪" )
-          currencySymbol.sub("JPY","¥").sub( "MYR", "MYR" ).sub( "MXN", "MX$" )
-          currencySymbol.sub("NOK","NOK").sub( "NZD", "NZ$" ).sub( "PHP", "Php" )
-          currencySymbol.sub("PLN","PLN").sub( "GBP", "£" ).sub( "SGD", "SGD" )
-          currencySymbol.sub("SEK","SEK").sub( "CHF", "CHF" ).sub( "TWD", "NT$" )
-          currencySymbol.sub("THB","฿").sub( "TRY", "TRY" ).sub( "USD", "$" )
+          currencySymbol = currencySymbol.sub("AUD","AUD $").sub( "BRL", "R$" ).sub( "CAD", "CAD $" )
+          currencySymbol = currencySymbol.sub("CZK","CZK").sub( "DKK", "DKK" ).sub( "EUR", "€" )
+          currencySymbol = currencySymbol.sub("HKD","HK$").sub( "HUF", "HUF" ).sub( "ILS", "₪" )
+          currencySymbol = currencySymbol.sub("JPY","¥").sub( "MYR", "MYR" ).sub( "MXN", "MX$" )
+          currencySymbol = currencySymbol.sub("NOK","NOK").sub( "NZD", "NZ$" ).sub( "PHP", "Php" )
+          currencySymbol = currencySymbol.sub("PLN","PLN").sub( "GBP", "£" ).sub( "SGD", "SGD" )
+          currencySymbol = currencySymbol.sub("SEK","SEK").sub( "CHF", "CHF" ).sub( "TWD", "NT$" )
+          currencySymbol = currencySymbol.sub("THB","฿").sub( "TRY", "TRY" ).sub( "USD", "$" )
         else
           number_to_currency(price.to_f / 100)
         end
