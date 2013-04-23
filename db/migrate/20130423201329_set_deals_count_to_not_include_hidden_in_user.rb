56866701d439af512aae6eb96ea22a05ce359b32
Michael Scaria <michaelscaria@yahoo.com>
2013-04-23 18:45:49 -0500

    add not to lowercase and

--- db/migrate/20130423201329_set_deals_count_to_not_include_hidden_in_user.rb
+++ db/migrate/20130423201329_set_deals_count_to_not_include_hidden_in_user.rb
@@ -1,6 +1,6 @@
 class SetDealsCountToNotIncludeHiddenInUser < ActiveRecord::Migration
   def self.up
-    ActiveRecord::Base.connection.execute("update users set deals_count = (select count(id) from deals where deals.user_id = users.id and deals.hidden)")
+    ActiveRecord::Base.connection.execute("update users set deals_count = (select count(id) from deals where deals.user_id = users.id and NOT deals.hidden)")
   end
 #deals.user_id = users.id AND d
   def self.down
