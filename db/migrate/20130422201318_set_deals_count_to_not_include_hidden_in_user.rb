f52567cf9008fd79a8b96b796b1f7acfd06e1db9
Michael Scaria <michaelscaria@yahoo.com>
2013-04-22 21:36:46 -0500

    revert deal count changes

--- db/migrate/20130422201318_set_deals_count_to_not_include_hidden_in_user.rb
+++
@@ -1,8 +0,0 @@
-class SetDealsCountToNotIncludeHiddenInUser < ActiveRecord::Migration
-  def self.up
-    ActiveRecord::Base.connection.execute("update users set deals_count = (select count(id) from deals where NOT deals.hidden)")
-  end
-#deals.user_id = users.id AND d
-  def self.down
-  end
-end
