756083848fca1c1c3341b620871a39ad16a4a123
Michael Scaria <michaelscaria@yahoo.com>
2013-05-17 17:08:40 -0500

    add push devices to admin to check

---
+++ app/admin/push_devices.rb
@@ -0,0 +1,12 @@
+ActiveAdmin.register PushDevices do
+
+  
+  index do
+    column("ID"){|device| device.id.try(:to_s)}
+    column("User ID"){|device| device.user.id.try(:to_s)}
+    column :token 
+    default_actions
+  end
+  
+
+end
