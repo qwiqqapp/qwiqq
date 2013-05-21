331369dc53e1685f5ae226744d1292328cb291fd
Michael Scaria <michaelscaria@yahoo.com>
2013-05-20 19:31:39 -0500

    try striping again

--- app/models/user.rb
+++ app/models/user.rb
@@ -488,7 +488,7 @@ class User < ActiveRecord::Base
       return if push_token.blank?
       search = String.new(push_token)
       puts "String:#{search}"
-      search = search.upcase
+      search = search.upcase.strip
       search[' '] = ''
       @device = PushDevice.where(:token => search).first
       puts "Test push token:#{search} if push device exists:#{@device}"
