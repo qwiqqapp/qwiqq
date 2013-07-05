7636a39229f0c0094d78836b6718b3277d36a0e4
Michael Scaria <michaelscaria@yahoo.com>
2013-07-04 20:42:55 -0700

    test

--- db/migrate/20130704101125_add_lat_long_to_users.rb
+++ db/migrate/20130704101125_add_lat_long_to_users.rb
@@ -1,8 +1,8 @@
 class AddLatLongToUsers < ActiveRecord::Migration
 
   def change
-    remove_column :user, :lat
-    remove_column :user, :lon
+    remove_column :users, :lat
+    remove_column :users, :lon
     add_column :users, :lat, :integer
     add_column :users, :lon, :integer
 
